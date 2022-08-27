b31ec8f0fc66667ce71631a40a76e546acfddc3d
amitayas <amitayasb@gmail.com>
2022-08-27 22:06:52 +0530

    modify 8

--- assignment2/8_v2.c
+++ assignment2/8_v2.c
@@ -1,25 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-
 void rotate(int *mat, int order, int gap) {
   int tmp1 = mat[0 * gap + order - 1];
-  //printf("\n%d\n", tmp1);
   for(int i = order - 2 ; i >= 1 ; i--) {
     mat[0 * gap + (i+1)] = mat[0 * gap + i];
   }
   int tmp2 = mat[(order - 1) * gap + order - 1];
-  //printf("\n%d\n", tmp2);
   for(int i = order - 2 ; i >= 1 ; i--) {
     mat[(i+1) * gap + order - 1] = mat[i * gap + order - 1];
   }
   int tmp3 = mat[(order - 1) * gap + 0];
-  //printf("\n%d\n", tmp3);
   for(int i = 1 ; i < order - 1 ; i++) {
     mat[(order - 1) * gap + i - 1] = mat[(order - 1) * gap + i];
   }
   int tmp4 = mat[0];
-  //printf("\n%d\n", tmp4);
   for(int i = 1 ; i < order - 1 ; i++){
     mat[(i-1) * gap + 0] = mat[i * gap + 0];
   }
@@ -42,15 +37,12 @@ void display(int *mat, int rows, int columns, int gap) {
 }
 
 int main() {
-  ///int mat[3][3] = {
-  ///  {1, 2, 3},
-  ///  {4, 5, 6},
-  ///  {7, 8, 9}
-  ///};
-  int *mat = (int*)malloc(sizeof(int) * 36);
-  for(int i = 0 ; i < 36 ; i++) {
-    scanf("%d", (mat + i));
-  }
-  rotate(mat, 6, 6);
-  display(mat, 6, 6, 6);
+  int mat[3][3] = {
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9}
+  };
+  int n = 2;
+  while(n-- > 0) rotate(mat, 3, 3);
+  display(mat, 3, 3, 3);
 }
